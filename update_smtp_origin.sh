d59cac1ae3ae20e04062cbfdc83adda16c4241ea
Fouille <franckisys@gmail.com>
2020-03-31 17:55:20 +0200

    all update

--- update_smtp_origin.sh
+++ update_smtp_origin.sh
@@ -6,35 +6,126 @@ whiptail --title "Hello Wazo HELP / Modify SMTP Origin" --msgbox "Your SMTP orig
 }
 
 update_smtp_origin() {
-SMTP_Origin=$(whiptail --title "Hello Wazo HELP / Modify SMTP Origin" --inputbox "Please write your new SMTP Origin Domain (ex.: smtp.wazo.io):" 13 60 3>&1 1>&2 2>&3)
+SMTP_Origin=$(whiptail --title "Hello Wazo HELP / Modify SMTP Origin" --inputbox "Please write your new SMTP Origin Domain (ex.: contact@domain.io):" 13 60 3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-{
-    echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
-    sudo -u postgres psql asterisk -c "UPDATE mail SET origin='$SMTP_Origin';";
+  {
+      echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
+      sudo -u postgres psql asterisk -c "UPDATE mail SET origin='$SMTP_Origin';";
 
-    echo -e "XXX\n50\nAdd $SMTP_Origin in DB... Done.\nXXX"
-    sleep 0.5
+      echo -e "XXX\n50\nAdd $SMTP_Origin in DB... Done.\nXXX"
+      sleep 0.5
 
-    echo -e "XXX\n50\nCreate new config... \nXXX"
-    xivo-create-config
-
-    echo -e "XXX\n100\nCreate Config... Done.\nXXX"
-    sleep 0.5
-
-    echo -e "XXX\n50\nUpdate new config... \nXXX"
-    xivo-update-config
-
-    echo -e "XXX\n100\nUpdate Config... Done.\nXXX"
-    sleep 0.5
-
-} | whiptail --gauge "Wait Please" 6 60 0
-
-bye
+  } | whiptail --gauge "Wait Please" 6 60 0
 
 fi
 
+update_domain
+
 }
 
+update_domain() {
+  domain=$(whiptail --title "Hello Wazo HELP / Modify Domain" \
+--inputbox "Please write domain (ex.: domain.tdl):" 13 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    {
+        echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
+        sudo -u postgres psql asterisk -c "UPDATE mydomain SET canonical='$domain';";
+
+        echo -e "XXX\n50\nAdd in DB... Done.\nXXX"
+        sleep 0.5
+
+    } | whiptail --gauge "Wait Please" 6 60 0
+  fi
+
+update_canonical
+
+}
+
+update_canonical() {
+Canonical=$(whiptail --title "Hello Wazo HELP / Modify Canonical" \
+--inputbox "Please write canical mail (ex.: ex@mail.com general@domain.tdl\r+ex2@mail.com general@domain.tdl):" 13 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    {
+        echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
+        sudo -u postgres psql asterisk -c "UPDATE mail SET canonical='$Canonical';";
+
+        echo -e "XXX\n50\nAdd in DB... Done.\nXXX"
+        sleep 0.5
+
+    } | whiptail --gauge "Wait Please" 6 60 0
+  fi
+
+  update_relayhost
+
+}
+
+update_relayhost() {
+Relayhost=$(whiptail --title "Hello Wazo HELP / Modify relayhost" \
+--inputbox "Please write Relay Host (ex.: smtp2.domain.tld | no relay? leave empty):" 13 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    {
+        echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
+        sudo -u postgres psql asterisk -c "UPDATE relayhost SET canonical='$Relayhost';";
+
+        echo -e "XXX\n50\nAdd in DB... Done.\nXXX"
+        sleep 0.5
+
+    } | whiptail --gauge "Wait Please" 6 60 0
+  fi
+
+  update_fallback_relayhost
+  
+}
+
+update_fallback_relayhost() {
+  fallback_relayhost=$(whiptail --title "Hello Wazo HELP / Modify fallback_relayhost" \
+--inputbox "Please write fallback relay host (ex.: smtp.provider.tld | no relay? leave empty):" 13 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    {
+        echo -e "XXX\n0\nAdd in DB, Please Wait... \nXXX"
+        sudo -u postgres psql asterisk -c "UPDATE fallback_relayhost SET canonical='$fallback_relayhost';";
+
+        echo -e "XXX\n50\nAdd in DB... Done.\nXXX"
+        sleep 0.5
+
+    } | whiptail --gauge "Wait Please" 6 60 0
+  fi
+
+  final_step
+
+}
+
+
+final_step() {
+  exitstatus=$?
+  if [ $exitstatus = 0 ]; then
+    {
+        echo -e "XXX\n50\nCreate new config... \nXXX"
+        xivo-create-config
+
+        echo -e "XXX\n100\nCreate Config... Done.\nXXX"
+        sleep 0.5
+
+        echo -e "XXX\n50\nUpdate new config... \nXXX"
+        xivo-update-config
+
+        echo -e "XXX\n100\nUpdate Config... Done.\nXXX"
+        sleep 0.5
+
+    } | whiptail --gauge "Wait Please" 6 60 0
+
+  bye
+
+  fi
+}
 update_smtp_origin
